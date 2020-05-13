55645039f35cea6ae40fdb93f9b83ca1cdd3a626
unrealshadow <yousafzai.kamran60@gmail.com>
2020-05-13 21:34:37 +0500

    initial commit

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Button playBtn;
+    public Button facebookBtn;
+    public Button leaderBoardBtn;
+    public Button likeBtn;
+    public Button shopBtn;
+    public Button settingsBtn;
+
+
+    public void OnButtonClicked(string message)
+    {
+        Debug.Log(message);
+    }
+}
